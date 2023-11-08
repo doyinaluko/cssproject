67532eac4ae982f57e033ae1f0473039275b69ac
doyinaluko <127832641+doyinaluko@users.noreply.github.com>
2023-11-08 13:50:25 +0100

    update

---
+++ shared.js
@@ -0,0 +1,60 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+// Method 1;
+// console.dir(backdrop.style['background-image']);
+// Either of the two methods will work when we need to target CSS elements from javascript
+// Method 2;
+// console.dir(backdrop.style.backgroundImage);
+
+// console.dir(selectPlanButton);
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener("click", function () {
+        // modal.style.display = "block";
+        // backdrop.style.display = "block";
+
+        // modal.className = 'open'; This will actually overwrite the complete class list
+
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+
+
+    });
+}
+
+backdrop.addEventListener("click", function() {
+    // mobileNav.style.display = 'none';
+
+    mobileNav.classList.remove('open');
+     closeModal();
+});
+
+if (modalNoButton) {
+modalNoButton.addEventListener("click", closeModal);
+
+}
+
+function closeModal() {
+    // backdrop.style.display = "none";
+    // modal.style.display = "none";
+    if (modal) {
+    modal.classList.remove('open');
+
+    }
+    backdrop.classList.remove('open');
+
+}
+
+toggleButton.addEventListener('click', function() {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+
+});
